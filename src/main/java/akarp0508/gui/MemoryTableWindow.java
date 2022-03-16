9699f4cb7c2d3003cb93854c412fff73c389872b
akarp0508 <akarp0508@gmail.com>
2022-03-16 17:26:33 +0100

    started work on MemoryTableWindow

--- src/main/java/akarp0508/gui/MemoryTableWindow.java
+++ src/main/java/akarp0508/gui/MemoryTableWindow.java
@@ -17,9 +17,9 @@ public class MemoryTableWindow {
 
     public MemoryTableWindow() {
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEtchedBorder(), "ODI Rankings", TitledBorder.CENTER, TitledBorder.TOP));
+        JPanel panel = new JPanel(new BorderLayout());
+        //panel.setBorder(BorderFactory.createTitledBorder(
+        //        BorderFactory.createEtchedBorder(), "ODI Rankings", TitledBorder.CENTER, TitledBorder.TOP));
         String[][] rec = {
                 { "1", "Steve", "AUS" },
                 { "2", "Virat", "IND" },
@@ -27,17 +27,110 @@ public class MemoryTableWindow {
                 { "4", "David", "AUS" },
                 { "5", "Ben", "ENG" },
                 { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" },
+                { "1", "Steve", "AUS" },
+                { "2", "Virat", "IND" },
+                { "3", "Kane", "NZ" },
+                { "4", "David", "AUS" },
+                { "5", "Ben", "ENG" },
+                { "6", "Eion", "ENG" }
         };
-        String[] header = { "Rank", "Player", "Country" };
+        String[] header = { "Rank", "Player", "Country" ,"a","a","a","a"};
 
         JTable table = new JTable(rec,header);
         panel.add(new JScrollPane(table));
-
         MyTableModel tableModel = new MyTableModel(rec,header);
 
+        //making menubar todo: add listeners
+        JMenuBar menubar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add(new JMenuItem("Read from file"));
+        fileMenu.add(new JMenuItem("Write to file"));
+        menubar.add(fileMenu);
+
+
+        JMenu formatMenu = new JMenu("Values format");
+        formatMenu.add(new JMenuItem("Binary"));
+        formatMenu.add(new JMenuItem("Decimal"));
+        formatMenu.add(new JMenuItem("Hexadecimal"));
+        menubar.add(formatMenu);
+
+        panel.add(menubar,BorderLayout.NORTH);
         frame.add(panel);
         table.setModel(tableModel);
-        frame.setSize(550, 400);
+        //frame.setSize(550, 400);
         frame.setVisible(true);
     }
 
@@ -48,11 +141,6 @@ public class MemoryTableWindow {
 
         private MyTableModel(String[][] rec, String[] header) {
             super(rec,header);
-        }
-
-        @Override
-        public Class<?> getColumnClass(int columnIndex) {
-            return Long.class;
         }
 
         @Override
